db971ea1a4a0cd08e42037516660c87a2b9d3de5
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-04-16 20:29:22 -0300

    feat: create model and migration clients

---
+++ back_end/src/migrations/20220416230907-create-client.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Clients', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cpf: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      }
+    });
+  }, 
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Clients');
+  }
+};
