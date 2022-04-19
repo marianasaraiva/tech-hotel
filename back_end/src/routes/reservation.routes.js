f98d400f2762752428159e80a5ce847a7023974d
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-04-19 09:52:00 -0300

    feat: validate the reservation file of this route using Joi

--- back_end/src/routes/reservation.routes.js
+++ back_end/src/routes/reservation.routes.js
@@ -1,10 +1,11 @@
 const reservationRouter = require('express').Router();
 const reservationController = require('../controllers/reservation');
 const auth = require('../middlewares/auth');
+const validationReservation = require('../middlewares/reservation');
 
 reservationRouter.route('/')
   .get()
-  .post(auth, reservationController.create);
+  .post(validationReservation, auth, reservationController.create);
   
 reservationRouter.route('/:id')
   .get()
