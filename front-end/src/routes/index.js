c62dc5897bd3e2eaaa853613a804d655c20d8bed
Mariana Saraiva <marianascm8@gmail.com>
2022-04-20 09:34:52 -0300

    feat: create pages Login and Register

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from '../pages/Login';
-import Cadastro from '../pages/Cadastro';
+import Cadastro from '../pages/Register';
 
 function Routes() {
   return (
