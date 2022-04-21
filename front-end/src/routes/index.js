e310cf00cefefd3dc2c38f9519edb4012d0cb8c4
Mariana Saraiva <marianascm8@gmail.com>
2022-04-21 16:19:49 -0300

    feat: home page created

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Contact from '../pages/Contact';
+import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Reservation from '../pages/Reservation';
@@ -8,6 +9,7 @@ import Reservation from '../pages/Reservation';
 function RoutesComponent() {
   return (
     <Routes>
+      <Route path="/" element={ <Home/> } />
       <Route path="/login" element={ <Login/> } />
       <Route path="/register" element={ <Register/> } />
       <Route path="/reservation" element={ <Reservation/> } />
