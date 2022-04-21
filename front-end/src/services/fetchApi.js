686edecf4b792bbb15560bbb3c1d4157c7393c39
nathaliamiranda <mirandanathaliaf@gmail.com>
2022-04-21 11:09:09 -0300

    refactor: added headers parameter in the function

--- front-end/src/services/fetchApi.js
+++ front-end/src/services/fetchApi.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
 
-const fetchAPI = async (method, url, data) => {
+const fetchAPI = async (method, url, data = {}, headers = {}) => {
   const response = await axios({
     method,
     url,
     data,
+    headers,
   });
 
   return response;
