d50ca9fedfc8d58fcc9d731f306c7be430ac005f
Mariana Saraiva <marianascm8@gmail.com>
2022-04-23 19:18:35 -0300

    style: tableReservation component styling

--- front-end/src/components/TableReservation/index.js
+++ front-end/src/components/TableReservation/index.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import Context from '../../context/Context';
+import { Table } from './styles';
 
 
 function TableReservation() {
@@ -7,32 +8,30 @@ function TableReservation() {
 
   return (
     doneReservation.length !== 0 &&
-      <div>
-        <table aling="center" border="1">
-            <thead>
-              <tr>
-                <th>Nome completo</th>
-                <th>E-mail</th>
-                <th>Quarto</th>
-                <th>Check-in</th>
-                <th>Estadia</th>
-                <th>Preço total da reserva</th>
+      <Table>
+          <thead>
+            <tr>
+              <th>Nome completo</th>
+              <th>E-mail</th>
+              <th>Quarto</th>
+              <th>Check-in</th>
+              <th>Estadia</th>
+              <th>Preço total da reserva</th>
+            </tr>
+          </thead>
+          <tbody>
+            { doneReservation.map((e, i) => (
+              <tr key={ i }>
+                <td>{ e.client.fullName }</td>
+                <td>{ e.client.email }</td>
+                <td>{ e.rooms[0].type }</td>
+                <td>{ e.checkIn }</td>
+                <td>{ `${e.quantityDays} dias` }</td>
+                <td>{ `R$ ${e.totalPrice},00` }</td>
               </tr>
-            </thead>
-            <tbody>
-              { doneReservation.map((e, i) => (
-                <tr key={ i }>
-                 <td>{ e.client.fullName }</td>
-                 <td>{ e.client.email }</td>
-                 <td>{ e.rooms[0].type }</td>
-                 <td>{ e.checkIn }</td>
-                 <td>{ `${e.quantityDays} dias` }</td>
-                 <td>{ `R$ ${e.totalPrice},00` }</td>
-               </tr>
-              )) }
-            </tbody>
-          </table>
-      </div>
+            )) }
+          </tbody>
+        </Table>
   )
 }
 
