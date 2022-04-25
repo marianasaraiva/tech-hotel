04bab2462fe40c0d40b6f7d229608f95f9eb8942
Mariana Saraiva <marianascm8@gmail.com>
2022-04-25 18:25:49 -0300

    feat: add url in constants file

--- front-end/src/utils/constants.js
+++ front-end/src/utils/constants.js
@@ -1,4 +1,6 @@
 export const url= {
+  CLIENT: 'http://localhost:3001/client',
+  LOGIN: 'http://localhost:3001/login', 
   ROOM: 'http://localhost:3001/room',
   RESERVATION: 'http://localhost:3001/reservation',
 };
