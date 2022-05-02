9702ccfde4664044ef7f2c0a833edec4e8aebd39
Jonatas Passos <jpc080496@gmail.com>
2022-05-02 11:21:17 -0300

    feat: creating application and server file (index.js)

--- back_end/src/api/app.js
+++ back_end/src/api/app.js
@@ -7,12 +7,12 @@ app.use(cors());
 
 app.use(express.json());
 
-const routes = require('./routes');
+const routes = require('../routes');
 
-const error = require('./middlewares/error');
+const error = require('../middlewares/error');
 
 app.use(routes);
 
 app.use(error);
 
-app.listen(process.env.SERVER_PORT || 3001, () => console.log('ouvindo porta 3001!'));
+module.exports = app;
